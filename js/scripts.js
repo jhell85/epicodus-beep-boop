3882eedc954d72e31f6487ded0556637629478e9
josh <jhell85@gmail.com>
2020-01-24 14:13:30 -0800

    got branching to work as supposed to

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,37 @@ $(document).ready(function() {
   //Back end
   var counter = 0;
   var numbers = [0];
-  var c = 1
+  var c = 1;
   function addNumber(number) {
-    counter ++;
-    numbers.push(counter);
     while (number > counter) {
-      addNumber(counter);
+      counter++;
+      numbers.push(counter);
     }
+
+    console.log(numbers);
+
     for (let i = 0; i < numbers.length; i++) {
-      splitCompare(numbers[i])
+      splitCompare(numbers[i]);
     }
-    function splitCompare(number){
-      var stringNumber = String(number).split("")
-      console.log( c + "//" + stringNumber)
-      c ++
+    function splitCompare(number) {
+      var stringNumber = String(number).split("");
+      console.log(c + "//" + stringNumber);
+      c++;
       for (let i = 0; i < stringNumber.length; i++) {
-        if (stringNumber[i] === "3"){
-          stringNumber = "I'm sorry, Dave. I'm afraid I can't do that."
-          // console.log(stringNumber)
-          break
-        } else if (stringNumber[i] === "2"){
-          stringNumber = "Boop"
-          break
-        } else if (stringNumber[i] === "1"){
-          stringNumber = "Beep"
-          break
-        } 
-        
+        if (stringNumber[i] === "3") {
+          stringNumber = "I'm sorry, Dave. I'm afraid I can't do that.";
+          console.log(stringNumber)
+          break;
+        } else if (stringNumber[i] === "2") {
+          stringNumber = "Boop";
+          console.log(stringNumber)
+          break;
+        } else if (stringNumber[i] === "1") {
+          stringNumber = "Beep";
+          console.log(stringNumber)
+          break;
+        }
       }
     }
   }
-
 });
