e8b00865d6b2a9c936c8d248d2fcfdcba840e5f5
josh <jhell85@gmail.com>
2020-01-24 13:48:11 -0800

    added if else to branch outcome of array

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,37 @@ $(document).ready(function() {
     addNumber(number);
   });
 
-  //Backend
+  //Back end
   var counter = 0;
   var numbers = [0];
+  var c = 1
   function addNumber(number) {
     counter ++;
     numbers.push(counter);
     while (number > counter) {
       addNumber(counter);
     }
-    console.log(numbers);
     for (let i = 0; i < numbers.length; i++) {
-      splitNumber(numbers[i])
-      // const element = array[i];
+      splitCompare(numbers[i])
     }
-    function splitNumber(number){
-      console.log(String(number))
+    function splitCompare(number){
       var stringNumber = String(number).split("")
-      console.log(stringNumber)
+      console.log( c + "//" + stringNumber)
+      c ++
+      for (let i = 0; i < stringNumber.length; i++) {
+        if (stringNumber[i] === "3"){
+          stringNumber = "I'm sorry, Dave. I'm afraid I can't do that."
+          // console.log(stringNumber)
+          break
+        } else if (stringNumber[i] === "2"){
+          stringNumber = "Boop"
+          break
+        } else if (stringNumber[i] === "1"){
+          stringNumber = "Beep"
+          break
+        } 
+        
+      }
     }
   }
 
