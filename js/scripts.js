89c1d080a1e8c08ac489b566eef9170e5c59d874
josh <jhell85@gmail.com>
2020-02-02 10:46:12 -0800

    seperated back end from front end in submitDocument function added a reset button after program runs

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,6 @@
-// Front end
-$(document).ready(function() {
-  $("#numberForm").submit(function(event) {
-    event.preventDefault();
-    var numberInput = parseInt($("#numberInput").val());
-    var name = $("#nameInput").val();
-    submitDocument(numberInput, name);
-  });
-});
-
-function printToDom(mutatedArray) {
-  $("#result-div").append("<ol class='result-list' start='0'></ol>");
-  mutatedArray.forEach(number => {
-    $("#result-div ol").append("<li>" + number + "</li>");
-  });
-}
 //Back end
-var counter = 0;
-var numbers = [0];
+
 function submitDocument(numberInput, name) {
-  $("#startContainer").addClass("none");
-  $("#results").removeClass("none");
-  $("body").removeClass("start");
   if (name === ""){
     name = generateName();
   }
@@ -59,5 +39,28 @@ function splitCompare(number, name,) {
 
 function generateName() {
   var randomNames = ["Nikol Whitebeard", "Charming Deanna Grey", "Cutthroat Anton The Charming", "First Mate Deanna The Stable", "First Mate Travis The Intuitive", "Cap'n Intuitive", "Gracious Brooke The Pirate", "Joss Gingerbeard", "Joss De Portland" ];
-  return randomNames[Math.floor(Math.random() * 10)];
+  return randomNames[Math.floor(Math.random() * 9)];
+}
+// Front end
+$(document).ready(function() {
+  $("#numberForm").submit(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($("#numberInput").val());
+    var name = $("#nameInput").val();
+    submitDocument(numberInput, name);
+  });
+  
+  $("#button-reload").click(function(){
+    location.reload(true)
+  });
+});
+
+function printToDom(mutatedArray) {
+  $("#startContainer").addClass("none");
+  $("#results").removeClass("none");
+  $("body").removeClass("start");
+  $("#result-div").append("<ol class='result-list' start='0'></ol>");
+  mutatedArray.forEach(number => {
+    $("#result-div ol").append("<li>" + number + "</li>");
+  });
 }
